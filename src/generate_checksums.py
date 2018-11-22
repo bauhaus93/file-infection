8629ab73af82e1f450db8c8a9e063eb35ef28b6d
Jakob Fischer <JakobFischer93@gmail.com>
2018-11-22 16:28:17 +0100

    Updated checksum generation script.

--- src/generate_checksums.py
+++ src/generate_checksums.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python2
-import sys
+import logging
+import argparse
+
+def setup_logger():
+    FORMAT = r"[%(asctime)-15s] %(levelname)s - %(message)s"
+    DATE_FORMAT = r"%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(level = logging.INFO, format = FORMAT, datefmt = DATE_FORMAT)
 
 def create_checksum(string):
     a = 1
@@ -11,34 +16,45 @@ def create_checksum(string):
 
     return ((b << 16) + a) & 0xFFFFFFFF
 
-argc = len(sys.argv)
-if argc == 1:
-    args = ["checksums.txt", "checksum_list.h", "CHECKSUM_LIST_H"]
-elif argc != 4:
-    print "Usage: filenameIn filenameOut safeguard"
-    exit(1)
-else:
-    args = sys.argv[1:]
+def create_checksum_map(input_filename):
+    checksum_map = {}
+    with open(input_filename, "r") as f:
+        for line in f:
+            name = line.rstrip()    #was [:-2] \r\n
+            cs = create_checksum(name)
+            if cs in checksum_map :
+                if name != checksum_map[cs]:
+                    logger.warn("Found hash collision: {} with {} -> 0x{:08X}".format(name, checksum_map[cs], cs))
+                else:
+                    logger.warn("Found duplicate string: {}".format(name))
+            else:
+                checksum_map[cs] = name
+    return checksum_map
 
-checksum = {}
-with open(args[0], "r") as f:
-    for line in f:
-        name = line[:-2]    #\r\n
-        cs = create_checksum(name)
-        if cs in checksum:
-            print "found hash collision: %s with %s -> 0x%08X" % (name, checksum[cs], cs)
-            exit(2)
-        checksum[cs] = name
+def create_header_file(header_filename, checksum_map):
+    with open(header_filename, "w") as f:
+        f.write("#ifndef {}\n#define {}\n".format(safeguard, safeguard))
+        for cs, name in sorted(checksum_map.items(), key = lambda e: e[1].lower()):
+            f.write("#define CS_{} (0x{:X})\n".format(name.upper().replace(".", "_"), cs))
+            logger.debug("Checksum for {}: 0x{:X}".format(name, cs))
+        f.write("#endif // {}".format(safeguard))
 
-l = []
-for key in checksum.keys():
-    l.append((checksum[key], key))
-l.sort(key = lambda e: e[0])
+if __name__ == "__main__":
+    setup_logger()
+    logger = logging.getLogger()
 
-with open(args[1], "w") as f:
-    f.write("#ifndef %s\n#define %s\n" % (args[2], args[2]))
-    for e in l:
-        f.write("#define CS_%s (0x%X)\n" % (e[0].upper().replace(".", "_"), e[1]))
-    f.write("#endif")
+    parser = argparse.ArgumentParser("Generate checksums from strings in a file, creating a c header files with #defines of the checksums.")
+    parser.add_argument("--input-file",
+                        metavar = "FILE",
+                        help = "Specify the file input file, containing one string per line.",
+                        required = True)
+    parser.add_argument("--output-file",
+                        metavar = "FILE",
+                        help = "Specify the output file, being a c header file.",
+                        required = True)
+    args = parser.parse_args()
 
-print "generated %s checksums" % len(l)
+    safeguard = args.output_file.upper().replace(".", "_")
+    checksum_map = create_checksum_map(args.input_file)
+    create_header_file(args.output_file, checksum_map)
+    logger.info("Created '{}', containing {} checksums".format(args.output_file, len(checksum_map)))
