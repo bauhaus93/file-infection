267cd482036245a7548b045cecfb3f587635ca91
Jakob Fischer <jakobfischer93@gmail.com>
2020-02-11 23:40:55 +0100

    Changes for qemu script

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-qemu-system-x86_64 -m 4096 -drive file=$PWD/vms/win10_snapshot.qcow2,format=qcow2 --smp cpus=4 -enable-kvm -nic none
+
+SHARED_DIR="$PWD/shared"
+
+rm -rf $SHARED_DIR && \
+mkdir -p $SHARED_DIR && \
+qemu-system-x86_64 \
+    -m 4096 \
+    --smp cpus=4 -enable-kvm \
+    -drive file=$PWD/vms/win10_snapshot.qcow2,format=qcow2 \
+    -net user,smb=$SHARED_DIR \
+    -net nic,model=virtio
+
